e2112771a2032a982d2b65997dae48c330790db4
Casey Hutchinson <hutchster@gmail.com>
2016-05-11 10:11:00 -0400

    Add grid system and style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page.</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out:</p>
+			<ol>
+			  <li>
+					<Link to='/?location=Lexington'>Lexington, KY</Link>
+			  </li>
+			  <li>
+					<Link to='/?location=Philaelphia'>Philadelphia, PA</Link>
+			  </li>
+			  <li>
+					<Link to='/?location=Rio'>Rio, Brazil</Link>
+			  </li>
+			</ol>
 		</div>
 	)
 };
--- public/bundle.js
+++ public/bundle.js
@@ -24844,11 +24844,14 @@
 			null,
 			React.createElement(Nav, null),
 			React.createElement(
-				'h2',
-				null,
-				'Main'
-			),
-			props.children
+				'div',
+				{ className: 'row' },
+				React.createElement(
+					'div',
+					{ className: 'columns medium-6 large-4 small-centered' },
+					props.children
+				)
+			)
 		);
 	};
 
@@ -26368,19 +26371,55 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166);
+
+	var Link = _require.Link;
+
+
 	var Examples = function Examples(props) {
 		return React.createElement(
 			'div',
 			null,
 			React.createElement(
-				'h3',
-				null,
+				'h1',
+				{ className: 'text-center' },
 				'Examples'
 			),
 			React.createElement(
 				'p',
 				null,
-				'Welcome to examples page.'
+				'Here are a few example locations to try out:'
+			),
+			React.createElement(
+				'ol',
+				null,
+				React.createElement(
+					'li',
+					null,
+					React.createElement(
+						Link,
+						{ to: '/?location=Lexington' },
+						'Lexington, KY'
+					)
+				),
+				React.createElement(
+					'li',
+					null,
+					React.createElement(
+						Link,
+						{ to: '/?location=Philaelphia' },
+						'Philadelphia, PA'
+					)
+				),
+				React.createElement(
+					'li',
+					null,
+					React.createElement(
+						Link,
+						{ to: '/?location=Rio' },
+						'Rio, Brazil'
+					)
+				)
 			)
 		);
 	};
